5b14809b66fdf9b47950e0dfc73b6ede1224f9b4
Gulshank21umar <141059656+Gulshank21umar@users.noreply.github.com>
2024-05-13 06:29:28 +0000

    variable

---
+++ basic/variables.js
@@ -0,0 +1,11 @@
+const accountId=123456
+let accountEmail="gul123@gmail.com"
+var accountPassword="123"
+accountCity="chennai"
+let accountState
+accountEmail="gk@gmail.com"
+accountPassword="1111"
+accountCity="bihar"
+// accountId=2 // not allowed
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountState])
